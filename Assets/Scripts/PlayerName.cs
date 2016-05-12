e1631c078f53dfee64600d11d6ef9b29490d668a
Hausmans <shausman98@gmail.com>
2016-05-12 08:15:51 -0700

    Added hamsterball and hamster prefabs

---
+++ Assets/Scripts/PlayerName.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerName : MonoBehaviour {
+
+	private GameObject text;
+	private GameObject camera;
+	private Text name;
+
+	public Vector3 ballPosition;
+
+	// Use this for initialization
+	void Start () {
+		text = GameObject.Find("Text");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		ballPosition = GameObject.Find ("HamsterBall").transform.position;
+		text.transform.position = ballPosition + new Vector3(0.5f,0.7f,0.7f);
+	}
+}
